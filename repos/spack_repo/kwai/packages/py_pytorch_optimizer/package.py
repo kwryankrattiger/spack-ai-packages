a7f1cd152178bd4f5bbcc0dc75b62257fd714210
John Parent <john.parent@kitware.com>
2025-10-28 15:39:43 -0400

    Add py-pytorch-optimizer
    Signed-off-by: John Parent <john.parent@kitware.com>

---
+++ repos/spack_repo/kwai/packages/py_pytorch_optimizer/package.py
@@ -0,0 +1,31 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+
+from spack.package import (
+    maintainers,
+    license,
+    version,
+    depends_on,
+)
+
+
+class PyPytorchOptimizer(PythonPackage):
+    """optimizer & lr scheduler & objective function collections in PyTorch"""
+
+    homepage = "https://github.com/kozistr/pytorch_optimizer"
+    pypi = "pytorch_optimizer/pytorch_optimizer-3.8.2.tar.gz"
+
+    maintainers("kwryankrattiger", "johnwparent")
+
+
+    license("Apache-2.0", checked_by="johnwparent")
+
+    version("3.8.2", sha256="ec2a73dc1ecbd272c25f7caef15032c8b63f4103cd3e30529df76b39be589b08")
+
+    depends_on("py-poetry-core@2.0.0:", type="build")
+
+    depends_on("python@3.8:")
+    depends_on("py-torch@1.10:", when="python")
