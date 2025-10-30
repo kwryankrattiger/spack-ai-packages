80408ef4c0abf0c44b92cc565372460d484f3093
John Parent <john.parent@kitware.com>
2025-10-30 17:49:42 -0400

    Kitware copyright and remove template cruft
    Signed-off-by: John Parent <john.parent@kitware.com>

--- repos/spack_repo/kwai/packages/py_kwcoco/package.py
+++ repos/spack_repo/kwai/packages/py_kwcoco/package.py
@@ -1,23 +1,6 @@
-# Copyright Spack Project Developers. See COPYRIGHT file for details.
+# Copyright Kitware, Inc. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-kwcoco
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-kwcoco
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack_repo.builtin.build_systems.python import PythonPackage
 
