b4898e10e27a92b06d2a14ee13e9c0e35c4dddc1
John Parent <john.parent@kitware.com>
2025-10-30 13:38:07 -0400

    Update simple-dvc-version ranges
    Signed-off-by: John Parent <john.parent@kitware.com>

--- repos/spack_repo/kwai/packages/py_simple_dvc/package.py
+++ repos/spack_repo/kwai/packages/py_simple_dvc/package.py
@@ -1,4 +1,4 @@
-# Copyright Spack Project Developers. See COPYRIGHT file for details.
+# Copyright Kitware, Inc. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
@@ -27,22 +27,24 @@ class PySimpleDvc(PythonPackage):
 
     version("0.2.1", sha256="7b153e2c101525b467818e9cbb4024cdfb4ee414fb5770dba853e5144e418834")
 
-    # FIXME: Add a build backend, usually defined in pyproject.toml. If no such file
-    # exists, use setuptools.
-    # depends_on("py-setuptools", type="build")
-    # depends_on("py-hatchling", type="build")
-    # depends_on("py-flit-core", type="build")
-    # depends_on("py-poetry-core", type="build")
+    depends_on("py-setuptools", type="build")
 
-    depends_on("py-ubelt@1.3.3:")
-    depends_on("py-pandas")
-    depends_on("py-kwutil@0.2.3:")
-    depends_on("py-pyyaml@6.0.1:")
-    depends_on("py-ruamel-yaml@0.17.22:")
-    depends_on("py-argcomplete@1.0:")
+    depends_on("py-ubelt@1.3.3:", when="python@3.6:")
+    depends_on("py-pandas@1.4.0:", when="python@3.8:3.10")
+    depends_on("py-pandas@1.3.5:", when="python@3.10:3.11")
+    depends_on("py-pandas@1.5.0:", when="python@3.11:3.12")
+    depends_on("py-pandas@1.0.0:", when="python@3.6:3.7")
+    depends_on("py-pandas@1.2.0:", when="python@3.7:3.8")
+    depends_on("py-pandas@2.1.1:", when="python@4:3.12")
+    depends_on("py-pyyaml@6.0:", when="python@:3.12")
+    depends_on("py-pyyaml@6.0.1:", when="python@3.12:4")
+    depends_on("py-rich@11.2:", when="python@3.6:3.8")
+    depends_on("py-rich@12.4.4:", when="python@3.8:")
+    depends_on("py-scriptconfig@0.7.9:", when="python@3.8:")
     depends_on("py-packaging@21.3:")
-    depends_on("py-scriptconfig@0.7.9:")
-    depends_on("py-rich@12.4.4:")
+    depends_on("py-ruamel-yaml@0.17.22:")
+    depends_on("py-kwutil@0.2.3:")
+    depends_on("py-argcomplete@1.0:")
     depends_on("py-cmd-queue@0.1.18:")
     depends_on("py-dvc@3.7.0:")
 
